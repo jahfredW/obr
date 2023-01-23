4e065ddf6c47e40014460a0e7a41d103bfef634f
fredator <fred.gruwe@gmail.com>
2023-01-23 15:59:06 +0100

    ajout du formulaire de connexion - make:auth

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,17 +2,50 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegisterController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
-    public function index(): Response
+    private $entityManager;
+    private $hasher;
+
+    public function __construct(EntityManagerInterface $em, UserPasswordHasherInterface $hasher){
+        $this->entityManager = $em;
+        $this->hasher = $hasher;
+    }
+    #[Route('/inscription', name: 'app_register')]
+    public function index(Request $request): Response
     {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+        $form->handleRequest($request);
+
+        if( $form->isSubmitted() && $form->isValid() ){
+            $user = $form->getData();
+
+            $password = $this->hasher->hashPassword($user, $user->getPassword());
+
+            $user->setPassword($password);
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('app_home');
+
+        }
+
+
+
+
         return $this->render('register/index.html.twig', [
-            'controller_name' => 'RegisterController',
+            'form' => $form->createView()
         ]);
     }
 }
